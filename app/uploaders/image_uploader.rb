11af334f65e7c5d281d47d616624af2ea59352b0
shi-miya <colon9anje9@icloud.com>
2020-02-12 17:48:44 +0900

    メッセージ送信機能実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process convert: 'jpg'
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -30,21 +30,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [800, 800]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # version :thumb do
-  # process :resize_to_fit => [400, 200]
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
   # end
-   # 許可する画像の拡張子
-  def extension_white_list
-    %W[jpg jpeg gif png]
-  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-    def filename
-      "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
-  end
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+end
