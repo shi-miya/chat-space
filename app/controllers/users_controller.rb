1b4baf0c683e6be305a38eac7bcb1bc6800c497b
shi-miya <colon9anje9@icloud.com>
2020-02-17 14:33:24 +0900

    incremental-search add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
 
     respond_to do |format|
-      # format.html
+      format.html
       format.json
     end
   end
